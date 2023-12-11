b248dd60ce902e05305abe404a1237a88feb3e54
kadirkaradag <kadirkaradag0132@gmail.com>
2023-12-11 22:03:41 +0300

    Add project files.

---
+++ Core.Persistence/Repositories/Entity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Repositories;
+
+public class Entity<TId>
+{
+    public TId Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
+
+    public Entity() // hiç bir şey verilmezse, o id nin int se mesela default u 0 ise onu versin
+    {
+        Id = default;
+    }
+
+    public Entity(TId id)
+    {
+        Id= id;
+    }
+
+
+}

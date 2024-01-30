a3fba77155b46560963c73092e0151b3ea219703
kadirkaradag <kadirkaradag0132@gmail.com>
2024-01-30 19:22:15 +0300

    global exception handling

---
+++ Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
@@ -0,0 +1,16 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Types;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Handlers;
+
+public abstract class ExceptionHandler  //implementasyon bunu implemente edecek yerde olacak o yüzden abstract class olacak
+{
+    public Task HandleExceptionAsync(Exception exception) =>
+        exception switch
+        {
+            BusinessException businessException => HandleException(businessException), // gelen exception BusinessException businessException türündeyse HandleException(businessException), diğer caselerde _ kısmı calısacak.
+            _ => HandleException(exception)
+        };
+
+    protected abstract Task HandleException(BusinessException businessException); // abstract olmasının sebebi inherit eden sınıf doldurmak zorunda olsun
+    protected abstract Task HandleException(Exception exception); // diğer durumlarda bu calısacak
+}

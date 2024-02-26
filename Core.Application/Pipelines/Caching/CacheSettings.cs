145f749019e01adc0f1ec4bdf430ab5b74143d29
kadirkaradag <kadirkaradag0132@gmail.com>
2024-02-26 21:30:01 +0300

    Caching Substructure builded.

---
+++ Core.Application/Pipelines/Caching/CacheSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace Core.Application.Pipelines.Caching;
+
+public class CacheSettings // appsettings icin ayarlar
+{
+    public int SlidingExpiration { get; set; }
+}
---
+++ Core.Application/Pipelines/Caching/ICachableRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Pipelines.Caching;
+
+public interface ICachableRequest
+{
+    string CacheKey { get; }
+    bool BypassCache { get; }
+    TimeSpan? SlidingExpiration { get; }  // cache de tutulacak süre
+}

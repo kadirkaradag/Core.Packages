a99950c907a1698eea8eba144148de4783ed6103
kadirkaradag <kadirkaradag0132@gmail.com>
2024-02-28 21:39:49 +0300

    Logger service process.

---
+++ Core.CrossCuttingConcerns/SeriLog/LoggerServiceBase.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+
+namespace Core.CrossCuttingConcerns.SeriLog;
+
+public abstract class LoggerServiceBase // abstract olma sebebi, mongo istersem mongoya dosyaya istersem dosyaya loglama yapması icin
+{
+    protected ILogger Logger { get; set; }  //protected olmasının sebebi: abstract sadece inheritance ile kullanılabilir. bunu da sadece inherit eden yer görsün diye protected. protectedlar bu şekilde public gibi yazılır
+
+    protected LoggerServiceBase()
+    {
+        Logger = null;
+    }
+
+    protected LoggerServiceBase(ILogger logger)
+    {
+        Logger = logger;
+    }
+
+    public void Verbose(string message) => Logger.Verbose(message); //mesaj vermek istediğinde detaylı log icin
+    public void Fatal(string message) => Logger.Fatal(message);
+    public void Info(string message) => Logger.Information(message);
+    public void Warn(string message) => Logger.Warning(message);
+    public void Debug(string message) => Logger.Debug(message);
+    public void Error(string message) => Logger.Error(message);
+
+}
---
+++ Core.CrossCuttingConcerns/SeriLog/Loggers/FileLogger.cs
@@ -0,0 +1,30 @@
+﻿using Core.CrossCuttingConcerns.SeriLog.ConfigurationModels;
+using Core.CrossCuttingConcerns.SeriLog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace Core.CrossCuttingConcerns.SeriLog.Loggers;
+
+public class FileLogger : LoggerServiceBase
+{
+    private readonly IConfiguration _configuration; // appsettings den veri okuyabilmek icin
+
+    public FileLogger(IConfiguration configuration)
+    {
+        _configuration = configuration;
+
+        FileLogConfiguration logConfig = configuration.GetSection("SeriLogConfigurations:FileLogConfiguration").Get<FileLogConfiguration>() ?? throw new Exception(SeriLogMessages.NullOptionsMessage);
+        //get() ile mapping yapıyoruz. microsoft.binder kütüphanesi ile yapılıyor. yani git appsettings den veriyi al git bu neysene maple diyoruz.
+
+        string logFilePath = string.Format(format: "{0}{1}", arg0: Directory.GetCurrentDirectory() + logConfig.FolderPath, arg1: ".txt");
+
+        Logger = new LoggerConfiguration().WriteTo.File(
+            logFilePath,
+            rollingInterval: RollingInterval.Day,
+            retainedFileCountLimit:null,
+            fileSizeLimitBytes:5000000,
+            outputTemplate:"{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{level}] {Message}{NewLine}{Exception}"
+            ).CreateLogger();
+        //rollingInterval ne kadar sürede bir yeni dosya olustursun, retainedFileCountLimit dosyalar ne zaman silinsin,fileSizeLimitBytes dosya boyutu maks bu kadar olsun, outputTemplate nasıl tutulacak.
+    }
+}
